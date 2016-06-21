2a0f74bed1eff9001f6229158474ef96d69c232f
Lixan <Lixan.63@hotmail.fr>
2016-06-21 17:57:50 +0200

    English comments

--- src/main/scala/DataDisplay.scala
+++ src/main/scala/DataDisplay.scala
@@ -9,9 +9,9 @@ object DataDisplay extends ColumnDataSource with Tools{
 
 
   /**
-    * Initialise et affiche un graphique 1 dimension à partir de données présentes dans un vecteur
+    * Initialize and display a 1D graph from data in a vector
     *
-    * @param myData : vecteur de données
+    * @param myData : Vector with data
     */
   def displayVector1D(myData:Array[Double]): Unit={
 
@@ -30,15 +30,15 @@ object DataDisplay extends ColumnDataSource with Tools{
   }
 
   /**
-    * Initialise et affiche un graphique 2 dimensions à partir de données présentes dans une matrice
+    * Initialize and display a 2D graph from data in a matrix
     *
-    * @param myData : matrice de données (2 lignes et N colonnes)
+    * @param myData : Matrix with data (2 lines, N columns)
     */
   def displayMatrix2D(myData:Array[Array[Double]]): Unit={
 
     val plot = initializePlot()
-    val x = column(myData(0)) //données aléatoires
-    val y = column(myData(1)) //germes aléatoires
+    val x = column(myData(0)) //random data
+    val y = column(myData(1)) //random germs
 
     val xaxis = new LinearAxis().plot(plot).location(Location.Below)
     val yaxis = new LinearAxis().plot(plot).location(Location.Left)
@@ -55,13 +55,13 @@ object DataDisplay extends ColumnDataSource with Tools{
   }
 
   /**
-    * Affiche un vecteur de vecteur du type [ [germe1, algo1, res1.1], [germe2, algo1, res1.2], ... [germeN, algoN, resN.N] ]
+    * Display a vector of vector [ [germ1, algo1, res1.1], [germ2, algo1, res1.2], ... [germN, algoN, resN.N] ]
     *
-    * @param myData : le vecteur de vecteur à afficher
+    * @param myData : The vector of vector to display
     */
   def displayVectorOfVector(myData:Vector[Vector[Any]]): Unit={
 
-    /** Initialisation du graphique **/
+    /** Initialization of the graph **/
     val plot = initializePlot()
     val xaxis = new LinearAxis().plot(plot).location(Location.Below).axis_label("Résultats aléatoires")
     val yaxis = new LinearAxis().plot(plot).location(Location.Left).axis_label("Germes aléatoires")
@@ -72,17 +72,17 @@ object DataDisplay extends ColumnDataSource with Tools{
     var listRenderers = List(xaxis, yaxis)
     plot.renderers :=  listRenderers
 
-    /** Récupération des données **/
+    /** get data **/
     val colors = Array[Color](Color.Red,Color.Blue,Color.Green,Color.Yellow,Color.Orange,Color.Brown,
-      Color.Black, Color.Magenta, Color.Pink, Color.Purple) //Ajouter des couleurs si beaucoup de courbes à afficher
-    var indicColor = 0 //index du tableau color pour savoir quelle couleur afficher pour une courbe
+      Color.Black, Color.Magenta, Color.Pink, Color.Purple) //Add colors here if you have a lot of algorithms
+    var indicColor = 0 //Index of colors array to know which color to use to display a group of data from an algorithm
     var nomAlgo = ""
     var glyph = new Circle()
     var circle = new GlyphRenderer()
-    var leg = ListBuffer.empty[(String,List[GlyphRenderer])] //Légende du graphique : un nom de légende associé à des points
-    var data = Vector[Any]() //Vecteur contenant les informations du type [Double,String,Double]
-    val nbData=myData.length //nombre de données
-    var i=0 //itérateur des données
+    var leg = ListBuffer.empty[(String,List[GlyphRenderer])] //Legend of the graph: a name is associated with an algorithm
+    var data = Vector[Any]() //Vector with informations like [Double,String,Double]
+    val nbData=myData.length //Number of data
+    var i=0 //Iterator of data
 
     while(i < nbData)
     {
@@ -90,50 +90,50 @@ object DataDisplay extends ColumnDataSource with Tools{
 
       while(i < nbData && myData(i)(1).toString() == nomAlgo)
       {
-        //Récupération des données
+        //Get the data
         data = myData(i)
 
-        //Création d'un point
+        //Create a data point
         glyph = new Circle().x(data(0).asInstanceOf[Double]).y(data(2).asInstanceOf[Double]).size(5).fill_color(colors(indicColor)).line_color(Color.Black)
         circle = new GlyphRenderer().data_source(DataDisplay).glyph(glyph)
 
-        //ajout du point
+        //add the data point
         plot.renderers.<<=(_ :+ circle)
 
-        //passage à la donnée suivante
+        //next data
         i += 1
       }
 
-      leg = leg :+(nomAlgo, List(circle)) //ajout de la légende dans la liste des légendes (nom de légende associé une à liste de points)
-      indicColor = (indicColor + 1) % colors.length //changement de couleur si on a des valeurs provenant d'autres algo (permettra de différencier les courbes)
+      leg = leg :+(nomAlgo, List(circle)) //Add the legend to the list of legends (name of the legend associated to an algorithm)
+      indicColor = (indicColor + 1) % colors.length //Change the color because we change the algorithm
 
     }
 
-    val legend = new Legend().legends(leg.toList) //récupération de la liste des légendes afin de l'insérer
-    plot.renderers.<<=(_ :+ legend) //affichage de la légende
+    val legend = new Legend().legends(leg.toList) //Get list of legends in order to insert it
+    plot.renderers.<<=(_ :+ legend) //Display the legend
     webDisplay(plot)
   }
 
 
   /**
-    * Initialise un graphique
+    * Initialize a graph
     *
-    * @return : le graphique
+    * @return : The graph
     */
   def initializePlot(): Plot ={
     val xdr = new DataRange1d()
     val ydr = new DataRange1d()
 
-    //Options du graphique
+    //Options of the graph
     val plot = new Plot().x_range(xdr).y_range(ydr).tools(Pan|WheelZoom|Resize|Reset|PreviewSave)
 
     return plot
   }
 
   /**
-    * Affichage d'un graphique dans une page web
+    * Display a graph in a browser
     *
-    * @param plot : le graphique à afficher
+    * @param plot : The graph to display
     */
   def webDisplay(plot: Plot): Unit ={
     val document = new Document(plot)
